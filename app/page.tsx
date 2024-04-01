382293a586b5a154e7e2bb20510c2d1cf310609f
Ray-Hackshaw <ray.hackshaw@gmail.com>
2024-04-01 15:15:43 +1300

    fix: packages and cube spin

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { Mesh } from "three";
 
-export default function Home() {
- return (
-    <Canvas>
-    <ambientLight intensity={0.1} />
-    <directionalLight color="red" position={[0, 0, 5]} />
-    <mesh>
-      <boxGeometry />
+function Cube() {
+  const meshRef = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (!meshRef.current) return;
+    if (!meshRef.current.rotation) return;
+
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2, 2, 2]} />
       <meshStandardMaterial />
     </mesh>
-  </Canvas>
- )
+  );
+}
+
+export default function Home() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <Cube />
+    </Canvas>
+  );
 }
