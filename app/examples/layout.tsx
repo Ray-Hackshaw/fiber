178e7a5e187be6a37de01248429cfb7f70ad027d
Ray <ray@cubdigital.co.nz>
2024-04-18 14:14:32 +1200

    feat: work on layout

--- app/examples/layout.tsx
+++ app/examples/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Example {
   label: string;
   href: string;
@@ -41,16 +43,38 @@ const ExamplesLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <section className="h-screen flex">
-      <div className="border-2 space-y-4 border-black w-full max-w-md text-white p-4 bg-black">
-        <h1 className="text-xl">React Three Fiber - Component Library</h1>
+    <section className="h-screen flex p-4">
+      <div className="space-y-4 w-full max-w-md ">
+        <h1>
+          <Link className="text-xl text-white" href="/examples">
+            React Three Fiber - Component Library
+          </Link>
+        </h1>
         <div className="space-y-2 indent-2">
           {examples.map((x) => (
-            <div key={x.href}>{x.label}</div>
+            <div key={x.href}>
+              <Link
+                href={x.href}
+                className="text-white opacity-80 hover:opacity-100 transition-all duration-200 ease-in-out"
+              >
+                {x.label}
+              </Link>
+            </div>
           ))}
         </div>
       </div>
-      <div className="p-16">{children}</div>
+      <div className="w-full space-y-4">
+        <div className="w-full flex gap-4 justify-between items-center">
+          <div className="w-full border px-2 py-1 rounded-md cursor-pointer opacity-80 hover:opacity-100 transition-all duration-200 ease-in-out">
+            Search for something...
+          </div>
+          <div className="w-40 border-l h-2" />
+          <div>github</div>
+          <div>contact</div>
+          <div>company</div>
+        </div>
+        <div className="">{children}</div>
+      </div>
     </section>
   );
 };
--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from "next";
 
 const ExamplesLanding: NextPage = () => {
-  return <div>Examples here</div>;
+  return (
+    <div>
+      <h1 className="text-2xl">
+        Component example library using React Three Fiber (Three.js).
+      </h1>
+    </div>
+  );
 };
 
 export default ExamplesLanding;
