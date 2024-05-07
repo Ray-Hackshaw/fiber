36911fa20bb051c2bb97d8475505ce039529be34
Ray <ray@cubdigital.co.nz>
2024-05-07 17:33:56 +1200

    feat: cant remember what i was doing

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,18 +1,20 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
 
-export default function MyCanvas({ ...props }) {
+import { Canvas } from "@react-three/fiber";
+import { Preload } from "@react-three/drei";
+import { r3f } from "@/src/components/helpers/global";
+import * as THREE from "three";
+
+export default function Scene({ ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+    <Canvas
+      {...props}
+      onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
+    >
+      {/* @ts-ignore */}
+      <r3f.Out />
+      <Preload all />
     </Canvas>
   );
 }
-
-// is this working
